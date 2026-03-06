6e6c1fb851362c80933f33c137ad5925b28533a2
KevinHaltiner <kevin.haltiner@bluewin.ch>
2026-03-06 09:50:40 +0100

    bobby

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const toggle = document.querySelector('.nav-toggle');
-  const header = document.querySelector('header');
-
-  toggle.addEventListener('click', () => {
-    header.classList.toggle('open');
-  });
-});
